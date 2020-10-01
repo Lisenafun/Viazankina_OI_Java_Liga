37ce18907915ce039b59d97460b5b61fa78dece3
OlesiaViazankina <lisena.cmex@rambler.ru>
2020-10-01 16:21:10 +0300

    task dead_lock done

---
+++ lesson2/task_dead_lock/src/DeadLock.java
@@ -0,0 +1,44 @@
+public class DeadLock {
+
+    static class Friend implements Comparable<Friend> {
+        private final String name;
+
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void bow(Friend bower) {
+            System.out.format("%s: %s подстрелил меня!\n", this.name, bower.getName());
+            System.out.format("%s: стреляю в ответ!\n", this.name);
+            synchronized(compareTo(bower) > 0 ? bower : this) {
+                bower.bowBack(this);
+            }
+        }
+
+        public void bowBack(Friend bower) {
+            System.out.format("%s: %s подстрелил меня!\n", this.name, bower.getName());
+        }
+
+        @Override
+        public int compareTo(Friend o) {
+            return this.getName().compareTo(o.getName());
+        }
+    }
+
+    /**
+     * Точка входа в программу
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+        Friend alphonse = new Friend("Alphonse");
+        Friend gaston = new Friend("Gaston");
+
+        new Thread(() -> alphonse.bow(gaston)).start();
+        new Thread(() -> gaston.bow(alphonse)).start();
+    }
+}
